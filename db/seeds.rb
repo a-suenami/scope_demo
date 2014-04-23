6e52f69d5089df2fd2d47175cd90edeb42ca1392
Akira SUENAMI <a.suenami@gmail.com>
2014-04-23 10:01:17 +0900

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+member1 = Member.create(id: 1, name: 'Member1')
+member2 = Member.create(id: 2, name: 'Member2')
+
+Message.create(content: 'message1 for member1', member: member1)
+Message.create(content: 'message2 for member1', member: member1)
+Message.create(content: 'message3 for member1', member: member1)
+Message.create(content: 'message4 for member1', member: member1)
+Message.create(content: 'message5 for member1', member: member1)
+Message.create(content: 'message6 for member1', member: member1)
+Message.create(content: 'message7 for member1', member: member1)
+Message.create(content: 'message8 for member1', member: member1)
+
+Message.create(content: 'message1 for member2', member: member2)
+Message.create(content: 'message2 for member2', member: member2)
+Message.create(content: 'message3 for member2', member: member2)
+Message.create(content: 'message4 for member2', member: member2)
+Message.create(content: 'message5 for member2', member: member2)
